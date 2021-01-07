23411698a04bf45102b9afa0d62c2ec9f4eedb7e
Han Wang <wanghan815@outlook.com>
2021-01-07 21:26:32 +0800

    check file

--- product.py
+++ product.py
@@ -1,13 +1,85 @@
+import os # operating system, 
+
+读取档案
 products = []
+if os.path.isfile('products.csv'):#同样的资料夹有没有
+#path is a module,这个查一下就可以只要可以查就不用背
+	with open('products.csv','r', encoding='utf-8') as f:
+		for line in f:
+			if 'product, price' in line:
+				continue #跳到下一回
+			name, price = line.strip().split(',') 
+			products.append([name, price])
+	print(products)
+else:
+	print('file no found')
+
+
+#让用户输入
 while True:
 	name = input('please input the name of product:')
 	if name == 'q':
 		break
 	price = input('please input the price of products')
-	p = []
-	p.append(name)
-	p.append(price)
-	# p = [name, price]
-	products.append(p)
+	price = int(price)
+	products.append([name, price])
 print(products)
-products[0][0]
+
+for p in products:
+	print(p[0], 'price is', p[1])
+
+#read file,
+with open('products.csv', 'w', encoding='utf-8') as f:#help write Chinese) as f: # it is fine we dont have the file, because we are writing
+	f.write('product, price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')# combine them to a bigger str
+#when we add element, only same type of data could be added, and hence we need to convert price to str, or we cannot use +
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
